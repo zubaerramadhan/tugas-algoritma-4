046d4104701937becccb17203d64a1e928f55dbf
zubaerramadhan <zdn.zeei@gmail.com>
2024-10-27 13:52:15 +0700

    Update case 2.py

--- case 2.py
+++ case 2.py
@@ -2,9 +2,9 @@ Bil = input("masukkan suatu bilangan : ")
 
 try:
     bilangan = int(Bil)
-    if (bilangan) > 0:
+    if bilangan > 0:
         print("positif")
-    elif (bilangan) < 0:
+    elif bilangan < 0:
         print("negatif")
     else:
         print("nol")
